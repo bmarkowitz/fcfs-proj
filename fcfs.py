3e60c0f38cdcff4792366238598abe64df5dd052
Brett Markowitz <batting1000@comcast.net>
2018-10-10 12:13:19 -0400

    More setup

--- fcfs.py
+++ fcfs.py
@@ -1,1 +1,14 @@
 from data import *
+
+time = 0
+ready_queue  = []
+
+# All processes activated at time 0
+ready_queue.insert(0, p1)
+ready_queue.insert(0, p2)
+ready_queue.insert(0, p3)
+ready_queue.insert(0, p4)
+ready_queue.insert(0, p5)
+ready_queue.insert(0, p6)
+ready_queue.insert(0, p7)
+ready_queue.insert(0, p8)
--- process.py
+++ process.py
@@ -1,11 +1,15 @@
 # Class to define the structure of a single process
 
 class Process:
-    def __init__(self, bursts, io):
+    def __init__(self, bursts, io, name):
         self.bursts = bursts
         self.io = io
+        self.name = name
         self.current_burst = bursts[0]
         self.current_io = io[0]
         self.resp_time = 0
         self.wait_time = 0
         self.ta_time = 0
+
+    def __str__(self):
+        return self.name
